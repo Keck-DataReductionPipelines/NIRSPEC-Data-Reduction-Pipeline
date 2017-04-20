526d2b7ed1d5fcab122709e244e8c1874f149d1c
Astrodude11 <Astrodude11@users.noreply.github.com>
2017-04-20 08:17:08 -1000

    Update nirspec_constants.py
    Added AO filters to filter_names.

--- nirspec_constants.py
+++ nirspec_constants.py
@@ -5,7 +5,8 @@ N_ROWS = 1024
 LONG_SLIT_EDGE_MARGIN = 6
 
 filter_names = ['NIRSPEC-1', 'NIRSPEC-2', 'NIRSPEC-3', 
-                'NIRSPEC-4', 'NIRSPEC-5', 'NIRSPEC-6', 'NIRSPEC-7']
+                'NIRSPEC-4', 'NIRSPEC-5', 'NIRSPEC-6', 'NIRSPEC-7',
+                'NIRSPEC-1-AO', 'NIRSPEC-3-AO', 'NIRSPEC-5-AO', 'NIRSPEC-7-AO']
 
 starting_order = {'NIRSPEC-1': 80, 'NIRSPEC-2': 70, 'NIRSPEC-3': 67, 'NIRSPEC-4': 61, 
                   'NIRSPEC-5': 53, 'NIRSPEC-6': 49, 'NIRSPEC-7': 41 }
