dd91bf5a58919143db69816a2be4883a2c8ae6d0
Astrodude11 <acolson@keck.hawaii.edu>
2018-02-08 17:43:54 +0100

    Use LACOSMIC if astroscrappy can't be imported.
    Also added logging info for cosmic ray rejection method.

--- image_lib.py
+++ image_lib.py
@@ -5,6 +5,10 @@ import scipy.ndimage as ndimage
 import cosmics
 #from __builtin__ import None
 import os
+import logging
+
+
+logger = logging.getLogger('main')
 
 def rectify_spatial(data, curve):
     """
@@ -122,7 +126,18 @@ def cosmic_clean(data):
     sig_frac = 0.3
     obj_lim = 5.0
     
-    cosmicRayMask, cleanedArray = astroscrappy.detect_cosmics(data, pssl=0.0, gain=2.2, sigclip=sig_clip, sigfrac=sig_frac, objlim=obj_lim, readnoise=10.0, satlevel=np.inf, inmask=None, sepmed=False, cleantype='medmask', fsmode='median')
+    try:
+        import astroscrappy
+        msg = 'using Cython implementation of LACOSMIC'
+        cosmicRayMask, cleanedArray = astroscrappy.detect_cosmics(data, pssl=0.0, gain=2.2, sigclip=sig_clip, sigfrac=sig_frac, objlim=obj_lim, readnoise=10.0, satlevel=np.inf, inmask=None, sepmed=False, cleantype='medmask', fsmode='median')
+    except ImportError:
+        msg = 'using original LACOSMIC'
+        c = cosmics.cosmicsImage(data, sigclip=sig_clip, sigfrac=sig_frac, objlim=obj_lim, verbose=False)
+        c.run(max_iter)
+        cleanedArray = c.cleanarray
+
+    logger.info(msg)
+
     return(cleanedArray)
 
 def get_extraction_ranges(image_width, peak_location, obj_w, sky_w, sky_dist):
